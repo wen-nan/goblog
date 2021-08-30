699dcdc8f9ca8220e328bd3be156600b6a83f1f2
narcissus <1044532074@qq.com>
2021-08-30 16:46:39 +0800

    使用中间件

--- main.go
+++ main.go
@@ -7,18 +7,15 @@ import (
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprintf(w, "<h1>Hello, 欢迎来到goblog!<h1>")
 }
 
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
 		"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
 }
 
 func notFoundHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusNotFound)
 	fmt.Fprintf(w, "<h1>请求页面未找到 :(</h1><p>如有疑惑，请联系我们。</p>")
 }
@@ -37,6 +34,15 @@ func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "创建新的文章")
 }
 
+func forceHTMLMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// 1.设置标头
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		// 2.继续处理请求
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	// 使用第三方Mux
 	router := mux.NewRouter()
@@ -53,6 +59,9 @@ func main() {
 
 	// 自定义404页面
 	router.NotFoundHandler = http.HandlerFunc(notFoundHandler)
+
+	// 中间件：强制内容类型为HTML
+	router.Use(forceHTMLMiddleware)
 
 	// 通过命令路由获取URL示例
 	homeURL, _ := router.Get("home").URL()
