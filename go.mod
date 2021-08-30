7567ec658ba2a1e8f9e9dc4e2f788894ed651aea
narcissus <1044532074@qq.com>
2021-08-30 15:16:55 +0800

    集成 Gorilla Mux路由

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/creack/pty v1.1.15 // indirect
 	github.com/fatih/color v1.12.0 // indirect
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/mattn/go-isatty v0.0.13 // indirect
 	github.com/pelletier/go-toml v1.9.3 // indirect
 	golang.org/x/sys v0.0.0-20210823070655-63515b42dcdf // indirect
--- main.go
+++ main.go
@@ -2,18 +2,13 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
-func defaultHandler(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	if r.URL.Path == "/" {
-		fmt.Fprintf(w, "<h1>Hello, 这里是goblog 欢迎您</h1>")
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>"+
-			"<p>如有疑惑，请联系我们。</p>")
-	}
+	fmt.Fprintf(w, "<h1>Hello, 欢迎来到goblog!<h1>")
 }
 
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
@@ -22,12 +17,48 @@ func aboutHandler(w http.ResponseWriter, r *http.Request) {
 		"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
 }
 
-func main() {
-	// 使用自定义ServeMUX
-	router := http.NewServeMux()
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprintf(w, "<h1>请求页面未找到 :(</h1><p>如有疑惑，请联系我们。</p>")
+}
 
-	router.HandleFunc("/", defaultHandler)
-	router.HandleFunc("/about", aboutHandler)
-	
+func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	fmt.Fprintf(w, "文章ID：" + id)
+}
+
+func articlesIndexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "访问文章列表")
+}
+
+func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "创建新的文章")
+}
+
+func main() {
+	// 使用第三方Mux
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", homeHandler).Methods("GET").Name("home")
+	router.HandleFunc("/about", aboutHandler).Methods("GET").Name("about")
+
+	router.HandleFunc("/articles/{id:[0-9]+}",
+		articlesShowHandler).Methods("GET").Name("articles.show")
+	router.HandleFunc("/articles",
+		articlesIndexHandler).Methods("GET").Name("articles.index")
+	router.HandleFunc("/articles/{id:[0-9]+}",
+		articlesStoreHandler).Methods("POST").Name("articles.store")
+
+	// 自定义404页面
+	router.NotFoundHandler = http.HandlerFunc(notFoundHandler)
+
+	// 通过命令路由获取URL示例
+	homeURL, _ := router.Get("home").URL()
+	fmt.Println("homeURL:", homeURL)
+	articleURL, _ := router.Get("articles.show").URL("id", "23")
+	fmt.Println("articleURL:", articleURL)
+
 	http.ListenAndServe(":3000", router)
 }
